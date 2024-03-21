5c40703a7b16495451671e5106353dadd72e832c
DemetreOkhozoria <145259093+DemetreOkhozoria@users.noreply.github.com>
2024-03-21 19:39:06 +0400

    Add files via upload

---
+++ Day 48/homework/script.js
@@ -0,0 +1,29 @@
+let vault = 8
+let vault1 = 3
+
+console.log(vault < vault1)
+
+const bookname = "Elmwood"
+const bookpages = 214
+
+console.log(bookname,bookpages)
+
+const name = "Demetre"
+
+const age = 13
+
+let location = "Tbilisi"
+
+console.log(location,age,name)
+
+const num1 = 12
+
+const num2 = 16
+
+console.log(num1 + num2)
+
+console.log(num1 - num2)
+
+console.log(num1 * num2)
+
+console.log(num1 / num2)
---
+++ Day 48/script.js
@@ -0,0 +1,21 @@
+let num1 = 2
+let num2 = 3
+let num3 = 1
+
+num2 = 8
+num1 = 3
+console.log(num1,num2)
+
+console.log(num1*num2)
+
+console.log(num1 * num2 * num3 / 3)
+
+const namers2 = num2 / num3
+
+const numer1 = 5
+
+const numer2 = 3
+
+console.log(numer1 > numer2)
+
+console.log(num1 + num2 + num3)
